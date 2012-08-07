07165fc7c529159a0fec1d8dbf5d64323b6707df
Jeroen Coumans <jeroencoumans@gmail.com>
2012-08-07 12:32:36 +0200

    Fix most lint errors

--- js/app.js
+++ js/app.js
@@ -3,40 +3,112 @@ var APP = APP || {};
 /**
  * Core module for handling events and initializing capabilities
  */
-APP.core = (function() {
+APP.core = (function () {
+
+    "use strict";
 
     var loader,
-        html;
+        html,
+        navigationHeight,
+        viewportHeight,
+        wrapper,
+        content;
 
-    /***
-     * Initialize capabilities and attach listeners
+    /**
+     * Sets height of content based on height of navigation
      */
-    function init() {
+    function toggleNavigationHeight() {
 
-        loader = $("#loader");
-        html = $("html");
+        navigationHeight = $("#page-navigation").height();
+        viewportHeight = $(window).height();
+        wrapper = $("#page-wrapper");
+        content = $(".page-content");
 
-        initCapabilities();
-
-        // When used as standalone app or springboard app
-        if ($.supports.webapp) {
-            html.removeClass("website");
-            html.addClass("webapp");
+        if (html.hasClass("has-navigation") && navigationHeight > viewportHeight) {
+            wrapper.height(navigationHeight);
+            content.height(navigationHeight);
+        } else {
+            wrapper.height("");
+            content.height("");
         }
-
-        APP.events.init();
-
-        attachListeners();
-        attachGlobalListeners();
     }
+
+    /**
+     * Shows or hides the navigation menu
+     */
+    function toggleNavigation() {
+
+        html.toggleClass("has-navigation");
+    }
+
+    /**
+     * Hides the navigation menu
+    */
+    function hideNavigation() {
+
+        html.removeClass("has-navigation");
+    }
+
+    /**
+     * Shows the loader in an overlay
+     */
+    function showLoader() {
+
+        loader.show();
+    }
+
+    /**
+     * Hides the loader
+     */
+    function hideLoader() {
+
+        loader.hide();
+    }
+
+    /**
+     * Attach event listeners
+     */
+    function attachListeners() {
+
+        APP.events.attachClickHandler(".action-navigation-toggle", function (event) {
+            toggleNavigation();
+
+            if (!$.supports.webapp) {
+                toggleNavigationHeight();
+            }
+
+            event.preventDefault();
+
+            return false;
+        });
+
+        APP.events.attachClickHandler(".action-thumbnail-toggle", function (event) {
+            $('.webapp').toggleClass("has-thumbnail");
+        });
+
+
+        APP.events.attachClickHandler(".action-article", function (event) {
+            $('.webapp').toggleClass("has-article");
+        });
+
+
+    }
+
+    /**
+     * Attach event listeners for global (application) events
+     */
+    function attachGlobalListeners() {
+
+    }
+
 
     function initCapabilities() {
 
         $.supports = $.supports || {};
 
         // basic ios5 detection
-        $.os.ios5 = $.os.ios && $.os.version.indexOf("5.") != -1;
-        $.os.ios6 = $.os.ios && $.os.version.indexOf("6.") != -1;
+        $.os.ios5 = $.os.ios && $.os.version.indexOf("5.") !== -1;
+        $.os.ios6 = $.os.ios && $.os.version.indexOf("6.") !== -1;
 
         // Uncomment to test iOS5 mode
         // $.os.ios5 = true;
@@ -62,91 +134,26 @@ APP.core = (function() {
         $.supports.fastclick = $.supports.touch && !$.supports.pjax && (!$.os.ios5 && $.os.ios6);
     }
 
-    /**
-     * Attach event listeners
+    /***
+     * Initialize capabilities and attach listeners
      */
-    function attachListeners() {
+    function init() {
 
-        APP.events.attachClickHandler(".action-navigation-toggle", function(event) {
-            toggleNavigation();
+        loader = $("#loader");
+        html = $("html");
 
-            if (! $.supports.webapp) {
-                toggleNavigationHeight();
-            }
+        initCapabilities();
 
-            event.preventDefault();
-
-            return false;
-        });
-
-        APP.events.attachClickHandler(".action-thumbnail-toggle", function(event) {
-            $('.webapp').toggleClass("has-thumbnail");
-        });
-
-
-        APP.events.attachClickHandler(".action-article", function(event) {
-            $('.webapp').toggleClass("has-article");
-        });
-
-
-    }
-
-    /**
-     * Attach event listeners for global (application) events
-     */
-     function attachGlobalListeners() {
-
-     }
-
-    /**
-     * Sets height of content based on height of navigation
-     */
-     function toggleNavigationHeight() {
-
-        navigationHeight = $("#page-navigation").height();
-        viewportHeight = $(window).height();
-        wrapper = $("#page-wrapper");
-        content = $(".page-content");
-
-        if (html.hasClass("has-navigation") && navigationHeight > viewportHeight) {
-            wrapper.height(navigationHeight);
-            content.height(navigationHeight);
-        } else {
-            wrapper.height("");
-            content.height("");
+        // When used as standalone app or springboard app
+        if ($.supports.webapp) {
+            html.removeClass("website");
+            html.addClass("webapp");
         }
-     }
 
-    /**
-     * Shows or hides the navigation menu
-     */
-    function toggleNavigation() {
+        APP.events.init();
 
-        html.toggleClass("has-navigation");
-    }
-
-    /**
-     * Hides the navigation menu
-     */
-     function hideNavigation() {
-
-        html.removeClass("has-navigation");
-     }
-
-    /**
-     * Shows the loader in an overlay
-     */
-    function showLoader() {
-
-        loader.show();
-    }
-
-    /**
-     * Hides the loader
-     */
-    function hideLoader() {
-
-        loader.hide();
+        attachListeners();
+        attachGlobalListeners();
     }
 
     return {
@@ -163,23 +170,40 @@ APP.core = (function() {
  * Module for dealing with events, esp. preventing click events to happen
  * multiple times during animation or while content is loading.
  */
-APP.events = (function() {
+APP.events = (function () {
 
-    var EVENT_LOCK_TIMEOUT = 500;
-    var EVENT_LOCK_LOCKED = true;
-    var EVENT_LOCK_UNLOCKED = false;
+    "use strict";
 
-    var eventLock = EVENT_LOCK_UNLOCKED;
+    var EVENT_LOCK_TIMEOUT = 500,
+        EVENT_LOCK_LOCKED = true,
+        EVENT_LOCK_UNLOCKED = false,
+        eventLock = EVENT_LOCK_UNLOCKED,
+        body = $(document.body);
 
-    var body = $(document.body);
+    /**
+     * Whether the click on the given element should be intercepted.
+     * Currently only clicks with target "_blank" or "mailto"
+     * are NOT intercepted.
+     */
+    function isClickInterceptable(element) {
 
-    function init() {
+        element = $(element);
 
-        if ($.supports.fastclick) {
-            initFastClicks();
-        } else if ($.supports.pjax) {
-            interceptAnchorClicks();
-        }
+        return element.attr("target") !== "_blank" &&  element.attr("href").indexOf("mailto") !== 0;
+    }
+
+    /**
+     * Intercepts all clicks on anchor tags
+     */
+    function interceptAnchorClicks() {
+
+        body.on("click", "a", function (event) {
+            if (isClickInterceptable(this)) {
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                return false;
+            }
+        });
     }
 
     /**
@@ -190,9 +214,10 @@ APP.events = (function() {
 
         interceptAnchorClicks();
 
-        body.on("tap", "a", function(event) {
-            var href = $(event.target).closest("a").attr("href");
-            var prefix = "javascript";
+        body.on("tap", "a", function (event) {
+            var href = $(event.target).closest("a").attr("href"),
+                prefix = "javascript";
+
             prefix += ":"; // separated, otherwise jslint fails
             if (href && href.indexOf(prefix) !== 0) {
                 // tapped on an anchor with a full url
@@ -209,32 +234,6 @@ APP.events = (function() {
     }
 
     /**
-     * Intercepts all clicks on anchor tags
-     */
-    function interceptAnchorClicks() {
-
-        body.on("click", "a", function(event) {
-            if (isClickInterceptable(this)) {
-                event.preventDefault();
-                event.stopImmediatePropagation();
-                return false;
-            }
-        });
-    }
-
-    /**
-     * Whether the click on the given element should be intercepted.
-     * Currently only clicks with target "_blank" or "mailto"
-     * are NOT intercepted.
-     */
-    function isClickInterceptable(element) {
-
-        element = $(element);
-
-        return element.attr("target") !== "_blank" &&  element.attr("href").indexOf("mailto") !== 0;
-    }
-
-    /**
      * Enable or disables the event lock. The event lock prevents double tapping during animation.
      *
      * @param Boolean lock Whether to lock or unlock events: EVENT_LOCK_LOCKED or EVENT_LOCK_UNLOCKED
@@ -245,7 +244,7 @@ APP.events = (function() {
 
         eventLock = lock;
         if (eventLock === EVENT_LOCK_LOCKED && timeout !== 0) {
-            setTimeout(function() {
+            setTimeout(function () {
                 eventLock = EVENT_LOCK_UNLOCKED;
             }, timeout || EVENT_LOCK_TIMEOUT);
         }
@@ -274,7 +273,7 @@ APP.events = (function() {
      */
     function isLocked() {
 
-        return  eventLock === EVENT_LOCK_LOCKED;
+        return eventLock === EVENT_LOCK_LOCKED;
     }
 
     /**
@@ -287,12 +286,21 @@ APP.events = (function() {
     function attachClickHandler(selector, fn) {
 
         var eventType = $.supports.touch ? "tap" : "click";
-        body.on(eventType, selector, function(event) {
+        body.on(eventType, selector, function (event) {
             if (!APP.events.isLocked()) {
                 fn(event);
                 return false;
             }
         });
+    }
+
+    function init() {
+
+        if ($.supports.fastclick) {
+            initFastClicks();
+        } else if ($.supports.pjax) {
+            interceptAnchorClicks();
+        }
     }
 
     return {
@@ -327,9 +335,7 @@ APP.util = (function() {
         }
         var match = RegExp("[?&]" + name + "=([^&]*)").exec(queryString);
 
-        return match ?
-            decodeURIComponent(match[1].replace(/\+/g, " "))
-            : defaultValue;
+        return match ? decodeURIComponent(match[1].replace(/\+/g, " ")) : defaultValue;
     }
 
     return {
