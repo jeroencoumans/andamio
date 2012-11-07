a856edc0c38cb96627289af4db64aec54bc2c574
Jeroen Coumans <jeroencoumans@gmail.com>
2012-11-07 15:20:51 +0100

    Use require.js for JS loading

---
+++ js/main.js
@@ -0,0 +1,29 @@
+requirejs.config({
+    shim: {
+        'lib/zepto.scroll': {
+            deps: ['lib/zepto']
+        },
+        'app/events': {
+            deps: ['lib/zepto']
+        },
+        'app/util': {
+            deps: ['lib/zepto']
+        },
+        'app/init': {
+            deps: ['lib/zepto', 'app/events', 'app/util']
+        }
+    }
+})
+
+require(["lib/less", "lib/zepto", "lib/zepto.scroll", "lib/fastclick", "app/globals", "app/events", "app/util", "app/init"], function() {
+
+        $(document).ready(function() {
+            APP.core.init();
+
+            // Seed parent-view
+            $.get('style/page/page.html', function(response){
+                $("#parent-view .page-content").html(response);
+            });
+        });
+
+});
