53cd620a29ea9e688c9ebc750e8eb676f3346e9e
Jeroen Coumans <jeroencoumans@gmail.com>
2012-11-15 12:41:33 +0100

    Remove connection handling from phone

--- js/app/phone.js
+++ js/app/phone.js
@@ -4,41 +4,19 @@
 APP.phone = (function () {
 
     var APP_FROM_BACKGROUND_REFRESH_TIMEOUT = 30 * 60 * 1000,
-        lastUpdated = new Date(),
-        connection;
-
-    function isOnline(offline) {
-
-        // the Cordova event handlers will set the connection reported by Cordova
-        // if Cordova is not available, fall back to HTML5 or default to true
-        if (! $.supports.cordova) {
-
-            // check HTML5 property
-            connection = window.navigator.onLine;
-        } else if ("offline") {
-
-            // Useful for testing in offline mode
-            connection = false;
-        } else {
-
-            // By default, we assume an internet connection is present
-            connection = true;
-        }
-
-        return connection;
-    }
+        lastUpdated = new Date();
 
     /**
      * Intercepts all clicks on anchor tags
      */
     function interceptAnchorClicks() {
 
-        $(document.body).on("click", "a", function(event) {
-            if ($.supports.cordova && APP.util.isExternalLink(this)) {
+        $(document.body).on("click", "a", function() {
+            if (APP.util.isExternalLink(this)) {
 
                 // open external URL's in in-app Cordova browser
                 var href = $(this).attr("href");
-                navigator.utility.openUrl(href, "browser");
+                navigator.utility.openUrl(href, "popover");
                 return false;
             } else {
                 return true;
@@ -61,7 +39,7 @@ APP.phone = (function () {
 
             var pageScroller = $(".active-view .overthrow");
             $.scrollElement(pageScroller.get(0), 0);
-        });
+        }, false);
 
         // refresh when application is activated from background
         document.addEventListener("resign", function() {
@@ -74,28 +52,24 @@ APP.phone = (function () {
                 APP.open.refresh();
             }
         }, false);
+    }
 
-        // Support for online/offline mode
-        document.addEventListener("online", function() {
+    /**
+     * Init Cordova stuff. Only called when Cordova is actually loaded
+     */
+    function initCordova() {
 
-            connection = false;
-        }, false);
-
-        document.addEventListener("online", function() {
-
-            connection = true;
-        }, false);
+        interceptAnchorClicks();
+        attachListeners();
     }
 
     function init() {
 
-        interceptAnchorClicks();
-        // make sure to only attach the listners when Cordovia is ready
-        document.addEventListener("deviceready", attachListeners(), false);
+        // When Cordovia is loaded and talking to the device, initialize it
+        document.addEventListener("deviceready", initCordova(), false);
     }
 
     return {
-        "init": init,
-        "isOnline": isOnline
+        "init": init
     };
 })();
