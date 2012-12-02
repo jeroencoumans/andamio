cf681c171f42115679ee2de17afec48018a87526
Jeroen Coumans <jeroencoumans@gmail.com>
2012-12-02 16:49:31 +0100

    Add yuidoc-generated Javascript API

---
+++ doc/api/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "APP.alert",
+        "APP.connection",
+        "APP.core",
+        "APP.delay",
+        "APP.events",
+        "APP.loader",
+        "APP.modal",
+        "APP.nav",
+        "APP.open",
+        "APP.phone",
+        "APP.reveal",
+        "APP.search",
+        "APP.store",
+        "APP.tabs",
+        "APP.util",
+        "APP.views"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
