353775be6d891cd9944e5394aa6e4b42d9842754
Jeroen Coumans <jeroencoumans@gmail.com>
2013-01-23 14:11:31 +0100

    Separate capabilities module. This also makes it easier to initialise your own modules instead of calling APP.core.init()

--- dist/andamio-lib.js
+++ dist/andamio-lib.js
@@ -108,6 +108,64 @@ APP.delay = (function(){
     };
 })();
 /**
+ * Core module for initializing capabilities and modules
+ * @author Jeroen Coumans
+ * @class core
+ * @namespace APP
+ */
+APP.capabilities = (function () {
+
+    /**
+     * Initialize capabilities based on UA detection
+     * @method initCapabilities
+     */
+    function init() {
+
+        $.os = $.os || {};
+
+        // basic ios5 detection
+        $.os.ios5 = $.os.ios && $.os.version.indexOf("5.") !== -1;
+        $.os.ios6 = $.os.ios && $.os.version.indexOf("6.") !== -1;
+
+        // basic android detection
+        $.os.android2 = $.os.android && $.os.version >= "2" && $.os.version < "4"; // yes we also count android 3 as 2 ;-)
+        $.os.android4 = $.os.android && $.os.version >= "4" && $.os.version < "5";
+
+        // basic blackberry detection
+        $.os.bb10 = navigator.userAgent.indexOf("BB10") > -1;
+
+        $.supports = $.supports || {};
+        $.supports.cordova = navigator.userAgent.indexOf("TMGContainer") > -1;
+
+        $.supports.webapp = false;
+        $.supports.webapp =  APP.util.getQueryParam("webapp", false) === "1" || navigator.standalone || $.supports.cordova;
+
+        // Only enable for iPhone/iPad for now
+        $.supports.ftfastclick = $.os.ios;
+
+        // Setup HTML classes
+        var html = $("html");
+
+        // When used as standalone app or springboard app
+        if ($.supports.webapp) html.removeClass("website").addClass("webapp");
+        if ($.os.ios) html.addClass("ios");
+        if ($.os.ios5) html.addClass("ios5");
+        if ($.os.ios6) html.addClass("ios6");
+        if ($.os.android) html.addClass("android");
+        if ($.os.android2) html.addClass("android2");
+        if ($.os.android4) html.addClass("android4");
+
+        // TODO - Lazy media query
+        if (document.width >= 980) {
+            html.removeClass("website").addClass("webapp desktop no-touch");
+        }
+    }
+
+    init();
+
+})();
+
+/**
  * Module for dealing with events, esp. preventing click events to happen multiple times during animation or while content is loading.
  * @author Jeroen Coumans
  * @class events
@@ -1793,53 +1851,10 @@ APP.alert = (function () {
 APP.core = (function () {
 
     /**
-     * Initialize capabilities based on UA detection
-     * @method initCapabilities
-     */
-    function initCapabilities() {
-
-        $.os = $.os || {};
-
-        // basic ios5 detection
-        $.os.ios5 = $.os.ios && $.os.version.indexOf("5.") !== -1;
-        $.os.ios6 = $.os.ios && $.os.version.indexOf("6.") !== -1;
-
-        // basic android4 detection
-        $.os.android4 = $.os.android && $.os.version.indexOf("4.") !== -1;
-
-        $.supports = $.supports || {};
-        $.supports.cordova = navigator.userAgent.indexOf("TMGContainer") > -1;
-
-        $.supports.webapp = false;
-        $.supports.webapp =  APP.util.getQueryParam("webapp", false) === "1" || navigator.standalone || $.supports.cordova;
-
-        // Only enable for iPhone/iPad for now
-        $.supports.ftfastclick = $.os.ios;
-    }
-
-    /**
      * Initialize variables and attach listeners
      * @method init
      */
     function init(params) {
-        var html = $("html");
-
-        initCapabilities();
-
-        // When used as standalone app or springboard app
-        if ($.supports.webapp) {
-            html.removeClass("website");
-            html.addClass("webapp");
-        }
-
-        if ($.os.ios5) {
-            html.addClass("ios5");
-        }
-
-        // TODO - Lazy media query
-        if (document.width >= 980) {
-            html.removeClass("website").addClass("webapp desktop no-touch");
-        }
 
         APP.events.init();
 
@@ -1850,7 +1865,7 @@ APP.core = (function () {
         APP.nav.init();
         APP.modal.init();
         APP.reveal.init();
-        APP.store.init(params);
+        APP.store.init();
         APP.tabs.init();
         APP.views.init();
         APP.alert.init();
--- js/app/init.js
+++ js/app/init.js
@@ -7,53 +7,10 @@
 APP.core = (function () {
 
     /**
-     * Initialize capabilities based on UA detection
-     * @method initCapabilities
-     */
-    function initCapabilities() {
-
-        $.os = $.os || {};
-
-        // basic ios5 detection
-        $.os.ios5 = $.os.ios && $.os.version.indexOf("5.") !== -1;
-        $.os.ios6 = $.os.ios && $.os.version.indexOf("6.") !== -1;
-
-        // basic android4 detection
-        $.os.android4 = $.os.android && $.os.version.indexOf("4.") !== -1;
-
-        $.supports = $.supports || {};
-        $.supports.cordova = navigator.userAgent.indexOf("TMGContainer") > -1;
-
-        $.supports.webapp = false;
-        $.supports.webapp =  APP.util.getQueryParam("webapp", false) === "1" || navigator.standalone || $.supports.cordova;
-
-        // Only enable for iPhone/iPad for now
-        $.supports.ftfastclick = $.os.ios;
-    }
-
-    /**
      * Initialize variables and attach listeners
      * @method init
      */
     function init(params) {
-        var html = $("html");
-
-        initCapabilities();
-
-        // When used as standalone app or springboard app
-        if ($.supports.webapp) {
-            html.removeClass("website");
-            html.addClass("webapp");
-        }
-
-        if ($.os.ios5) {
-            html.addClass("ios5");
-        }
-
-        // TODO - Lazy media query
-        if (document.width >= 980) {
-            html.removeClass("website").addClass("webapp desktop no-touch");
-        }
 
         APP.events.init();
 
@@ -64,7 +21,7 @@ APP.core = (function () {
         APP.nav.init();
         APP.modal.init();
         APP.reveal.init();
-        APP.store.init(params);
+        APP.store.init();
         APP.tabs.init();
         APP.views.init();
         APP.alert.init();
