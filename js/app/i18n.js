e4798aedc03e9fcbb95db4eaaabd2eada8afed9b
Jeroen Coumans <jeroencoumans@gmail.com>
2013-03-01 14:35:56 +0100

    Move pager strings to i18n

--- js/app/i18n.js
+++ js/app/i18n.js
@@ -2,10 +2,13 @@
 /*global Andamio */
 
 Andamio.i18n = {
-    offlineMessage: "There was a problem. Is your connection working? <br>Please check and try again.",
     ajaxGeneralError: "Computer said no:",
     ajaxNotFound: "The page couldn't be found.",
     ajaxTimeout: "The server timed out waiting for the request.",
     ajaxServerError: "The server is having problems, try again later.",
-    ajaxRetry: "Please go back or try again."
+    ajaxRetry: "Please go back or try again.",
+    offlineMessage: "There was a problem. Is your connection working? <br>Please check and try again.",
+    pagerLoadMore: "Load more",
+    pagerLoading: "Loading&hellip;",
+    pagerNoMorePages: "There are no more items."
 };
