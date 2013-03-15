a7a37890a874216f48b30c119ae72681774024b4
Jeroen Coumans <jeroencoumans@gmail.com>
2013-03-15 18:55:20 +0100

    Add compress function to cache

--- js/app/cache.js
+++ js/app/cache.js
@@ -11,6 +11,31 @@ Andamio.cache = (function () {
 
     var cache;
 
+    function compress(s) {
+        var i, l, out = '';
+        if (s.length % 2 !== 0) s += ' ';
+        for (i = 0, l = s.length; i < l; i += 2) {
+            out += String.fromCharCode((s.charCodeAt(i) * 256) + s.charCodeAt(i + 1));
+        }
+
+        // Add a snowman prefix to mark the resulting string as encoded (more on this later)
+        return String.fromCharCode(9731) + out;
+    }
+
+    function decompress(s) {
+        var i, l, n, m, out = '';
+
+        // If not prefixed with a snowman, just return the (already uncompressed) string
+        if (s.charCodeAt(0) !== 9731) return s;
+
+        for (i = 1, l = s.length; i < l; i++) {
+            n = s.charCodeAt(i);
+            m = Math.floor(n / 256);
+            out += String.fromCharCode(m, n % 256);
+        }
+        return out;
+    }
+
     return {
 
         get: function (key) {
@@ -19,7 +44,7 @@ Andamio.cache = (function () {
                 var result = cache.get(key);
 
                 if (result) {
-                    return result;
+                    return decompress(result);
                 }
             }
         },
@@ -28,7 +53,7 @@ Andamio.cache = (function () {
 
             if (key && data && cache) {
                 var minutes = (typeof expiration === "number") ? expiration : Andamio.config.cacheExpiration;
-                cache.set(key, data, minutes);
+                cache.set(key, compress(data), minutes);
             }
         },
 
