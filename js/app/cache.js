a3cee5fd514479ed8c076432dd7bdd69e7524983
Jeroen Coumans <jeroencoumans@gmail.com>
2013-03-22 12:41:33 +0100

    Remove compress and decompress, it takes a long time on iPhone 4

--- js/app/cache.js
+++ js/app/cache.js
@@ -11,31 +11,6 @@ Andamio.cache = (function () {
 
     var cache;
 
-    function compress(s) {
-        var i, l, out = '';
-        if (s.length % 2 !== 0) s += ' ';
-        for (i = 0, l = s.length; i < l; i += 2) {
-            out += String.fromCharCode((s.charCodeAt(i) * 256) + s.charCodeAt(i + 1));
-        }
-
-        // Add a snowman prefix to mark the resulting string as encoded (more on this later)
-        return String.fromCharCode(9731) + out;
-    }
-
-    function decompress(s) {
-        var i, l, n, m, out = '';
-
-        // If not prefixed with a snowman, just return the (already uncompressed) string
-        if (s.charCodeAt(0) !== 9731) return s;
-
-        for (i = 1, l = s.length; i < l; i++) {
-            n = s.charCodeAt(i);
-            m = Math.floor(n / 256);
-            out += String.fromCharCode(m, n % 256);
-        }
-        return out;
-    }
-
     return {
 
         get: function (key) {
@@ -44,7 +19,7 @@ Andamio.cache = (function () {
                 var result = cache.get(key);
 
                 if (result) {
-                    return decompress(result);
+                    return result;
                 }
             }
         },
@@ -53,7 +28,7 @@ Andamio.cache = (function () {
 
             if (key && data && cache) {
                 var minutes = (typeof expiration === "number") ? expiration : Andamio.config.cacheExpiration;
-                cache.set(key, compress(data), minutes);
+                cache.set(key, data, minutes);
             }
         },
 
