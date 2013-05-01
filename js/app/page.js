5edc7f36965630197a93660bfb65ba3da4a54c12
Jeroen Coumans <jeroencoumans@gmail.com>
2013-05-01 15:10:23 +0200

    Some enhancements to page module - abort previous ajax requests, if any, and always call the callback function to show the ajax error, and pass the error type as second parameter to the callback

--- js/app/page.js
+++ js/app/page.js
@@ -1,16 +1,13 @@
 /*jshint es5: true, browser: true, undef:true, unused:true, indent: 4 */
 /*global Andamio, $ */
 
-Andamio.dom.refreshDate = $(".js-refresh-date");
-
 Andamio.page = (function () {
 
-    var updateTimestamp,
-        updateTimer;
+    var activeRequest = null;
 
     /**
      *
-     * @method doAjaxRequest
+     * @method doRequest
      * @private
      * @param url {String} the URL to load.
      * @param expiration {Number} the time (in minutes) to store the content
@@ -18,9 +15,44 @@ Andamio.page = (function () {
      * @param callback {Function} callback function that will be executed on success
      *
      */
-    function doAjaxRequest(url, expiration, cache, callback) {
+    function doRequest(url, expiration, cache, callback) {
 
-        $.ajax({
+        // If there are still requests pending, cancel them
+        if (activeRequest) {
+
+            activeRequest.abort();
+            activeRequest = null;
+        }
+
+        // Handle errors
+        function onError(xhr, type) {
+
+            // type is one of: "timeout", "error", "abort", "parsererror"
+            var status = xhr.status,
+                errorMessage = (type === "timeout") ? '<h3 class="alert-title">' + Andamio.i18n.offlineMessage + '</h3>': '<h3 class="alert-title">' + Andamio.i18n.ajaxGeneralError + '</h3><p>' + type + " " + status + '</p>',
+                errorHTML = '<div class="alert alert-error">' + errorMessage + '<a href="javascript:void(0)" class="button button-primary button-block action-refresh">' + Andamio.i18n.ajaxRetry + '</a>';
+
+            if (type === "timeout") {
+
+                Andamio.connection.goOffline();
+            }
+
+            callback(errorHTML, type);
+        }
+
+        function onSuccess(response) {
+
+            Andamio.connection.goOnline();
+            Andamio.cache.set(url, response, expiration);
+            callback(response);
+        }
+
+        function onComplete(xhr, status) {
+
+            activeRequest = null;
+        }
+
+        activeRequest = $.ajax({
             url: url,
             cache: cache,
             headers: {
@@ -28,76 +60,26 @@ Andamio.page = (function () {
                 "X-Requested-With": "XMLHttpRequest"
             },
 
-            error: function (xhr, type) {
-
-                // type is one of: "timeout", "error", "abort", "parsererror"
-                var status = xhr.status,
-                    errorMessage = '<a href="javascript:void(0)" class="action-refresh">' + Andamio.i18n.ajaxGeneralError + '<br>' + type + " " + status + '<br>' + Andamio.i18n.ajaxRetry + '</a>';
-
-                switch(type) {
-
-                case "timeout":
-                    Andamio.connection.goOffline();
-                    break;
-                case "error":
-                    if (Andamio.connection.status) {
-                        Andamio.alert.show(errorMessage);
-                    }
-                    break;
-                }
-            },
-
-            success: function (response) {
-                Andamio.connection.goOnline();
-                Andamio.cache.set(url, response, expiration);
-                callback(response);
-            }
+            error: onError,
+            success: onSuccess,
+            complete: onComplete
         });
     }
 
     return {
-        get lastUpdate() {
-            return updateTimestamp;
-        },
-
-        set lastUpdate(date) {
-
-            updateTimestamp = (date instanceof Date) ? date : new Date();
-            Andamio.dom.refreshDate.text(Andamio.util.relativeDate(updateTimestamp));
-        },
 
         load: function (url, expiration, cache, callback) {
 
-            clearInterval(updateTimer);
+            if (! url || ! $.isFunction(callback)) return;
 
-            var self = this,
-                doCallback = function (response) {
+            var cachedContent = Andamio.cache.get(url);
 
-                if (Andamio.dom.refreshDate.length > 0) {
-                    updateTimer = window.setInterval(function () {
-                        Andamio.dom.refreshDate.text(Andamio.util.relativeDate(self.lastUpdate));
-                    }, 60000);
-                }
+            if (cachedContent) {
 
-                if ($.isFunction(callback)) callback(response);
-            };
+                callback(cachedContent);
+            } else {
 
-            if (url) {
-
-                var cachedContent = Andamio.cache.get(url);
-
-                if (cachedContent) {
-
-                    self.lastUpdate = new Date(localStorage.getItem("lscache-" + url + "-cacheexpiration") * 60000 - (60000 * Andamio.config.cacheExpiration));
-                    doCallback(cachedContent);
-                } else {
-
-                    doAjaxRequest(url, expiration, cache, function (response) {
-
-                        self.lastUpdate = new Date();
-                        doCallback(response);
-                    });
-                }
+                doRequest(url, expiration, cache, callback);
             }
         },
 
