3484bd0f5510cf0837e904794aee4ce78c4a4725
Jeroen Coumans <jeroencoumans@gmail.com>
2013-06-05 11:55:19 +0200

    Update dist

--- dist/andamio.zepto.js
+++ dist/andamio.zepto.js
@@ -3866,7 +3866,7 @@ Andamio.pager = (function () {
         if (this.loading) return;
 
         // In website mode, the scroller is the window, but the viewport holds the actual scrollTop value
-        var scrollTop = Andamio.config.webapp ? this.scroller.scrollTop() : Andamio.dom.viewport.scrollTop();
+        var scrollTop = Andamio.views.currentView.scrollPosition;
 
         if (scrollTop + this.scrollerHeight + this.options.autoFetchThreshold >= this.scrollerScrollHeight) {
 
@@ -4351,101 +4351,107 @@ Andamio.nav = (function () {
 
 Andamio.pulltorefresh = (function () {
 
-    var isActive,
-        isLoading,
-        scrollTop,
-        options;
+    function PullToRefresh(view, options) {
 
-    function onTouchStart() {
-
-        // store the scrollTop to determine if we need to set a timeout or not
-        scrollTop = options.scroller[0].scrollTop;
-    }
-
-    function onTouchMove() {
-
-        // don't bother doing anything if the stored scrollTop wouldn't result in an actual PTR
-        if (scrollTop > options.offset) return;
-
-        scrollTop = options.scroller[0].scrollTop;
-
-        if (scrollTop < options.threshold) {
-
-            options.scroller.addClass("can-refresh");
-        } else {
-
-            options.scroller.removeClass("can-refresh");
+        if (! view.scroller) {
+            throw "view must have a scroller";
         }
+
+        this.view           = view;
+        this.scrollCache    = 0;
+        this.options        = options;
+        this.active         = false;
+        this.loading        = false;
+
+        this.enable();
     }
 
-    function onRefreshEnd() {
-
-        isLoading = false;
-        options.scroller.removeClass("is-refreshing");
-        options.callback();
-    }
-
-    function onTouchEnd() {
-
-        if (scrollTop > options.offset) return;
-
-        scrollTop = options.scroller[0].scrollTop;
-
-        if (scrollTop < options.threshold) {
-
-            isLoading = true;
-            options.scroller.addClass("is-refreshing").removeClass("can-refresh");
-            Andamio.views.currentView.content[0].innerHTML = "";
-            Andamio.loader.show();
-
-            // Set a hardcoded delay to actually refresh, since it sometimes happens so fast it causes a jarring experience
-            Andamio.util.delay(function () {
-                Andamio.views.refreshView(Andamio.views.currentView, null, onRefreshEnd);
-            }, 300);
-        }
-    }
-
-    return {
-
-        get status() {
-            return isActive;
-        },
-
-        get loading() {
-            return isLoading;
-        },
-
-        get options() {
-
-            return options;
-        },
+    PullToRefresh.prototype = {
 
         enable: function () {
 
-            isActive = true;
-            options.scroller.addClass("has-pull-to-refresh")
-                .on("touchmove", onTouchMove)
-                .on("touchstart", onTouchStart)
-                .on("touchend", onTouchEnd);
+            this.active = true;
+            this.loading = false;
+
+            this.view.scroller
+                .addClass("has-pull-to-refresh")
+                .on("touchmove",    $.proxy(this.onTouchMove, this))
+                .on("touchstart",   $.proxy(this.onTouchStart, this))
+                .on("touchend",     $.proxy(this.onTouchEnd, this));
+
         },
 
         disable: function () {
 
-            isActive = false;
-            options.scroller.removeClass("has-pull-to-refresh")
-                .off("touchmove", onTouchMove)
-                .off("touchstart", onTouchStart)
-                .off("touchend", onTouchEnd);
+            this.view.scroller
+                .removeClass("has-pull-to-refresh")
+                .off("touchmove",   $.proxy(this.onTouchMove, this))
+                .off("touchstart",  $.proxy(this.onTouchStart, this))
+                .off("touchend",    $.proxy(this.onTouchEnd, this));
+
+            this.active = false;
+            this.loading = false;
         },
 
-        init: function (params) {
+        onTouchStart: function () {
 
-            // Setup initial state
-            isActive = false;
+            // cache the scrollTop to determine if we need to set a timeout or not
+            this.scrollCache = this.view.scrollPosition;
+        },
+
+        onTouchMove: function () {
+
+            // don't bother doing anything if the stored scrollTop wouldn't result in an actual PTR
+            if (this.scrollCache > this.options.offset) return;
+
+            // start live listening
+            if (this.view.scrollPosition < this.options.threshold) {
+
+                this.view.scroller.addClass("can-refresh");
+            } else {
+
+                this.view.scroller.removeClass("can-refresh");
+            }
+        },
+
+        onTouchEnd: function () {
+
+            // cache the last scrollTop again
+            this.scrollCache = this.view.scrollPosition;
+
+            if (this.scrollCache > this.options.offset) return;
+
+            if (this.scrollCache < this.options.threshold) {
+
+                this.loading = true;
+                this.view.scroller.addClass("is-refreshing").removeClass("can-refresh");
+                Andamio.views.currentView.content[0].innerHTML = "";
+                Andamio.loader.show();
+
+                var onRefreshEnd = this.onRefreshEnd.call(this);
+
+                // Set a hardcoded delay to actually refresh, since it sometimes happens so fast it causes a jarring experience
+                Andamio.util.delay(function () {
+                    Andamio.views.refreshView(Andamio.views.currentView, null, onRefreshEnd);
+                }, 300);
+            }
+        },
+
+        onRefreshEnd: function () {
+
+            this.loading = false;
+            this.view.scroller.removeClass("is-refreshing");
+            this.options.callback();
+        }
+
+    };
+
+    return {
+
+        init: function (view, params) {
 
             // By default, we set the pull to refresh on the parentView
-            options = {
-                scroller  : Andamio.views.parentView.scroller,
+            var options = {
                 callback  : function () {},
                 threshold : -50,
                 offset    : 150
@@ -4453,7 +4459,7 @@ Andamio.pulltorefresh = (function () {
 
             $.extend(options, params);
 
-            this.enable();
+            return new PullToRefresh(view, options);
         }
     };
 })();
@@ -4778,6 +4784,7 @@ Andamio.views = (function () {
             }
 
             this.active = false;
+            this.scrollPosition = 0;
         }
     };
 
@@ -4807,6 +4814,15 @@ Andamio.views = (function () {
                 } else {
                     return Andamio.dom.win;
                 }
+            }
+        },
+        scrollPosition: {
+            get: function () {
+                return Andamio.config.webapp ? this.scroller[0].scrollTop : Andamio.dom.viewport[0].scrollTop;
+            },
+            set: function (value) {
+                if (Andamio.config.webapp && this.scroller.length) this.scroller[0].scrollTop = value;
+                else Andamio.dom.viewport[0].scrollTop = value;
             }
         },
         active: {
@@ -4854,8 +4870,8 @@ Andamio.views = (function () {
         set currentView(val)    { this.viewHistory.push(val); },
         get previousView()      { return Andamio.util.prev(this.viewHistory); },
 
-        get scrollPosition()    { return Andamio.util.last(this.scrollHistory); },
-        set scrollPosition(val) { this.scrollHistory.push(val); },
+        get previousScrollPosition()    { return Andamio.util.last(this.scrollHistory); },
+        set previousScrollPosition(val) { this.scrollHistory.push(val); },
 
         resetViews: function () {
 
@@ -4882,7 +4898,7 @@ Andamio.views = (function () {
                 view.url = url;
 
                 if (typeof scrollPosition === "number" && view.scroller.length) {
-                    view.scroller[0].scrollTop = scrollPosition;
+                    view.scrollPosition = scrollPosition;
                 }
 
                 if (! errorType) {
@@ -4939,7 +4955,7 @@ Andamio.views = (function () {
             this.currentView = view;
 
             if (this.previousView) {
-                this.scrollPosition = this.previousView.scroller[0].scrollTop;
+                this.previousScrollPosition = this.previousView.scrollPosition;
                 this.deactivateView(this.previousView);
             }
 
@@ -4965,7 +4981,7 @@ Andamio.views = (function () {
                 if (this.childCount === 1 || this.modalCount || this.mediaCount) {
                     this.activateView(this.currentView);
                 } else {
-                    this.activateView(this.currentView, this.currentUrl, false, this.scrollPosition);
+                    this.activateView(this.currentView, this.currentUrl, false, this.previousScrollPosition);
                 }
 
                 // Finally, delete the last scroll position
@@ -5043,7 +5059,11 @@ Andamio.views = (function () {
 
             // Don't open the same URL, instead refresh
             if (url === Andamio.views.currentUrl) {
-                this.currentView.scroller[0].scrollTop = 0;
+                if (Andamio.config.webapp) {
+                    $.scrollElement(this.currentView.scroller[0], 0);
+                } else {
+                    this.currentView.scrollPosition = 0;
+                }
 
                 return;
             }
